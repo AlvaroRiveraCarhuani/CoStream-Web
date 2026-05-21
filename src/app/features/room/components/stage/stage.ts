3985a2e6e13c0cb8fab7a87e1cf016d0f5708a7e
Rodrigo <rodri@google.com>
2026-05-20 23:03:00 -0400

    feat: layout dinamico para modo foco al compartir pantalla

--- src/app/features/room/components/stage/stage.ts
+++ src/app/features/room/components/stage/stage.ts
@@ -1,16 +1,20 @@
-import { Component, inject } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
+import { CommonModule } from '@angular/common'; // <-- 1. Importamos el módulo común
 import { RoomStateService } from '../../services/room-state.service';
 
 @Component({
   selector: 'app-stage',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule], // <-- 2. Lo agregamos a los imports
   templateUrl: './stage.html',
   styleUrl: './stage.css'
 })
 export class Stage {
   protected roomState = inject(RoomStateService);
 
-  // Esta función conecta la vista con el servicio
+  // Recibimos la orden de "Modo Foco" desde la barra inferior de la sala
+  @Input() isScreenSharing: boolean = false;
+
   subirAlEscenario(userId: string) {
     this.roomState.promoteToStage(userId);
   }
